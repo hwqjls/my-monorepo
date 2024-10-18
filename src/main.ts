a25fe5183bf4ee4f7002c82700c57229c4a6f033
胡武强 <1358949137@qq.com>
2024-10-18 15:16:28 +0800

    feat: 全拒引入element图标组件

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import router from './routers'
 import "@/styles/reset.scss";
 import "@/styles/common.scss";
 import 'element-plus/dist/index.css'
+import * as Icons from "@element-plus/icons-vue";
+
 import App from '@/App.vue'
 
-createApp(App)
-  .use(router)
+const app = createApp(App)
+
+// 注册element图标组件
+Object.keys(Icons).forEach(key => {
+  app.component(key, Icons[key as keyof typeof Icons])
+})
+
+app.use(router)
   .mount('#app')
