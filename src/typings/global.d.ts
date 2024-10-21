4623ca22e4115f419366b72d3ce0784b8c6efe47
胡武强 <1358949137@qq.com>
2024-10-21 16:59:51 +0800

    feat： 添加网络请求码以及全局声明

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -1,7 +1,22 @@
 /* Vite */
 declare interface ViteEnv {
-  VITE_ROUTER_MODE: 'hash' | 'history'
+  VITE_USER_NODE_ENV: "development" | "production" | "test";
+  VITE_PORT: number;
+  VITE_OPEN: boolean;
+  VITE_REPORT: boolean;
+  VITE_ROUTER_MODE: 'hash' | 'history';
+  VITE_BUILD_COMPRESS: "gzip" | "brotli" | "gzip,brotli" | "none";
+  VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE: boolean;
+  VITE_DROP_CONSOLE: boolean;
+  VITE_PWA: boolean;
+  VITE_DEVTOOLS: boolean;
+  VITE_PUBLIC_PATH: string;
+  VITE_API_URL: string;
+  VITE_PROXY: [string, string][];
+  VITE_CODEINSPECTOR: boolean;
 }
+
+declare type Recordable<T = any> = Record<string, T>;
 
 // 继承后就可以全局import.meta.env.引用到ViteEnv
 interface ImportMetaEnv extends ViteEnv {
