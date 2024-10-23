4232c6d3ce1228bfdf5b7b0f3cf0c6f157ca814f
胡武强 <1358949137@qq.com>
2024-10-23 11:49:14 +0800

    feat: 抽离vite.config.ts插件到build进行单独配置

---
+++ build/plugins.ts
@@ -0,0 +1,44 @@
+import { PluginOption } from "vite";
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import eslintPlugin from "vite-plugin-eslint";
+import { createHtmlPlugin } from "vite-plugin-html";
+
+const pathSrc = path.resolve(__dirname, 'src')
+
+/**
+ * 创建 vite 插件
+ * @param viteEnv
+ */
+export const createVitePlugins = (viteEnv: ViteEnv): (PluginOption | PluginOption[])[] => {
+  const { VITE_GLOB_APP_TITLE, VITE_REPORT, VITE_DEVTOOLS, VITE_PWA, VITE_CODEINSPECTOR } = viteEnv;
+  return [
+    vue(),
+    AutoImport({
+      imports: ['vue'],
+      resolvers: [ElementPlusResolver()],
+      eslintrc: {
+        enabled: false, // 启用与 Eslint 集成，生成后设置为false，需要重新生成才可打开
+        filepath: './.eslintrc-auto-imports.json', // 生成 EsLint 配置文件的路径，需要配置在eslint中避免eslint检查错误
+        globalsPropValue: true, // 用于覆盖 globals 属性，
+      },
+      dts: path.resolve(pathSrc, 'typings/auto-imports.d.ts')
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dts: path.resolve(pathSrc, 'typings/components.d.ts')
+    }),
+    // esLint 报错信息显示在浏览器界面上
+    eslintPlugin(),
+    // 注入变量到 html 文件
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: { title: VITE_GLOB_APP_TITLE }
+      }
+    }),
+  ]
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { ConfigEnv, defineConfig, loadEnv, UserConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { createVitePlugins } from "./build/plugins";
 import { wrapperEnv } from "./build/getEnv";
 import { createProxy } from "./build/proxy";
+import pkg from "./package.json";
+import dayjs from "dayjs";
 
-const pathSrc = path.resolve(__dirname, 'src')
+
+const { dependencies, devDependencies, name, version } = pkg;
+const __APP_INFO__ = {
+  pkg: { dependencies, devDependencies, name, version },
+  lastBuildTime: dayjs().format("YYYY-MM-DD HH:mm:ss")
+}
+
 
 
 // https://vitejs.dev/config/
@@ -15,10 +20,18 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   const root = process.cwd()
   const env = loadEnv(mode, root)
   const viteEnv = wrapperEnv(env)
-  
+
   return {
     base: viteEnv.VITE_PUBLIC_PATH,
     root,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, 'src')
+      }
+    },
+    define: {
+      __APP_INFO__: JSON.stringify(__APP_INFO__)
+    },
     css: {
       preprocessorOptions: {
         scss: {
@@ -34,22 +47,34 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       // Load proxy configuration from .env.development
       proxy: createProxy(viteEnv.VITE_PROXY)
     },
-    plugins: [
-      vue(),
-      AutoImport({
-        imports: ['vue'],
-        resolvers: [ElementPlusResolver()],
-        dts: path.resolve(pathSrc, 'typings/auto-imports.d.ts')
-      }),
-      Components({
-        resolvers: [ElementPlusResolver()],
-        dts: path.resolve(pathSrc, 'typings/components.d.ts')
-      })
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, 'src')
-      }
+    plugins: createVitePlugins(viteEnv),
+    esbuild: {
+      pure: viteEnv.VITE_DROP_CONSOLE ? ["console.log", "debugger"] : []
     },
+    build: {
+      outDir: "dist",
+      minify: "esbuild",
+      // esbuild 打包更快，但是不能去除 console.log，terser打包慢，但能去除 console.log
+      // minify: "terser",
+      // terserOptions: {
+      // 	compress: {
+      // 		drop_console: viteEnv.VITE_DROP_CONSOLE,
+      // 		drop_debugger: true
+      // 	}
+      // },
+      sourcemap: false,
+      // 禁用 gzip 压缩大小报告，可略微减少打包时间
+      reportCompressedSize: false,
+      // 规定触发警告的 chunk 大小
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          // Static resource classification and packaging
+          chunkFileNames: "assets/js/[name]-[hash].js",
+          entryFileNames: "assets/js/[name]-[hash].js",
+          assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
+        }
+      }
+    }
   }
 })
