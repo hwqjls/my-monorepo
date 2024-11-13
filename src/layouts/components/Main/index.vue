7121f5b406956833d9a803a89f3cad52bf20d651
胡武强 <1358949137@qq.com>
2024-11-13 14:53:40 +0800

    feat(layout): 🚀 添加主视图,使用map缓存keep-alive以及注入刷新页面方法

--- src/layouts/components/Main/index.vue
+++ src/layouts/components/Main/index.vue
@@ -1,15 +1,46 @@
 <template>
   <Maximize v-show="maximize" />
   <Tabs v-show="tabs" />
+  <el-main>
+    <router-view v-slot="{ Component, route }">
+      <transition appear name="fade-transform" mode="out-in">
+        <keep-alive :include="keepAliveName">
+          <component :is="createComponentWrapper(Component, route)" v-if="isRouterShow" :key="route.fullPath"></component>
+        </keep-alive>
+      </transition>
+    </router-view>
+  </el-main>
 </template>
 <script lang="ts" setup>
 import { storeToRefs } from "pinia";
 import { useGlobalStore } from "@/stores/modules/global";
+import { useKeepAliveStore } from "@/stores/modules/keepAlive";
 import Maximize from "./components/Maximize.vue";
 import Tabs from "@/layouts/components/Tabs/index.vue";
 
 const globalStore = useGlobalStore();
 const { maximize, tabs } = storeToRefs(globalStore);
+
+const keepAliveStore = useKeepAliveStore();
+const { keepAliveName } = storeToRefs(keepAliveStore);
+
+// 注入刷新页面方法
+const isRouterShow = ref(true);
+const refreshCurrentPage = (val: boolean) => (isRouterShow.value = val);
+provide("refresh", refreshCurrentPage);
+
+// 解决详情页 keep-alive 问题
+const wrapperMap = new Map();
+function createComponentWrapper(component: any, route: any) {
+  if (!component) return;
+  const wrapperName = route.fullPath;
+  let wrapper = wrapperMap.get(wrapperName);
+  if (!wrapper) {
+    wrapper = { name: wrapperName, render: () => h(component) };
+    wrapperMap.set(wrapperName, wrapper);
+  }
+  return h(wrapper);
+}
 
 // 监听当前页面是否最大化，动态添加 class
 watch(
