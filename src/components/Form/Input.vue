85267fb224c7c48e2175ee7acc3cd46d0f2f5d4b
胡武强 <1358949137@qq.com>
2024-11-19 14:35:14 +0800

    feat(component): 🚀 添加基于表单的Input组件

---
+++ src/components/Form/Input.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-form-item v-bind="$attrs" :prop="attrs.prop || attrs.field">
+    <el-input v-model="formData[$attrs.field as any]" v-bind="$attrs" />
+  </el-form-item>
+</template>
+<script lang="ts" setup>
+import { computed, defineProps } from "vue";
+
+const props = defineProps({
+  form: {
+    type: Object,
+    default: () => ({})
+  }
+});
+
+const attrs: any = useAttrs();
+const formData = computed(() => props.form);
+</script>
+<style lang="less" scoped></style>
